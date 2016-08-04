b888f1423137e77d41f5d5347edf6e7e38befa2c
Jack Sacane <sacanejack@gmail.com>
2016-08-04 01:56:15 -0400

    Fixed timing

--- NeoPixel_Earrings.ino
+++ NeoPixel_Earrings.ino
@@ -8,7 +8,8 @@
 #define MIC_PIN 1
 
 int animation = 0; // tracks current animation
-const long duration = 15000; // 15 seconds in milliseconds
+const long duration = 20000; // duration of each animation
+unsigned long start = millis(); // initialize global timer
 
 Adafruit_NeoPixel ring = Adafruit_NeoPixel(NUM_PIXELS, RING_PIN);
 
@@ -20,47 +21,51 @@ void setup()
 }
 
 void loop()
-{
-  unsigned long start = millis(); // reset timer
-  resetRing();
-  
-  while (millis() - start < duration) {
-    // switch animations every 30 seconds
-    switch(animation) {
-      case 0:
-        colorWipe(ring.Color(255, 0, 0), 50); // Red
-        colorWipe(ring.Color(0, 255, 0), 50); // Green
-        colorWipe(ring.Color(0, 0, 255), 50); // Blue
-        break;
+{  
+  // change animation every 20 seconds
+  switch(animation) {
+    case 0:
+      rgbWipe(50);
+      break;
 
-      case 1:
-        rainbow(30);
-        break;
+    case 1:
+      rainbow(30);
+      break;
 
-      case 2:
-        chase(40);
-        break;
+    case 2:
+      chase(50);
+      break;
 
-      case 3:
-        rainbowCycle(30);
-        break;
+    case 3:
+      rainbowCycle(30);
+      break;
 
-      case 4:
-        theaterChaseRainbow(60);
-        break;
-    }
+    case 4:
+      theaterChaseRainbow(60);
+      break;
   }
-  
-  animation = (animation + 1) % 5;
 }
 
-// Wipe through red/green/blue
-void colorWipe(uint32_t color, uint8_t wait)
+// Wipe through red/green/blue colors
+void rgbWipe(uint8_t wait)
 {
-  for (uint16_t i=0; i<ring.numPixels(); i++) {   
-    ring.setPixelColor(i, color);
-    ring.show();
-    delay(wait);
+  uint16_t i, j;
+  uint32_t colors[3] = { ring.Color(255,0,0), ring.Color(0,255,0), ring.Color(0,0,255) };
+  
+  for (j=0; j<3; j++) {
+
+    if (millis() - start >= duration) { // change animation every 15 seconds
+      animation = (animation + 1) % 5;
+      start = millis(); // reset timer
+      resetRing();
+      break;
+    }
+    
+    for (i=0; i<ring.numPixels(); i++) {
+      ring.setPixelColor(i, colors[j]);
+      ring.show();
+      delay(wait);
+    }
   }
 }
 
@@ -70,6 +75,13 @@ void rainbow(uint8_t wait)
   uint16_t i, j;
   
   for (j=0; j<256; j++) {
+
+    if (millis() - start >= duration) {
+      animation = (animation + 1) % 5;
+      start = millis();
+      resetRing();
+      break;
+    }
     
     for (i=0; i<ring.numPixels(); i++) {
       ring.setPixelColor(i, Wheel((i+j) & 255));
@@ -81,13 +93,20 @@ void rainbow(uint8_t wait)
 
 // Two white pixels chase each other around the ring
 void chase(uint8_t wait)
-{
+{  
   // initialize first two pixels
   ring.setPixelColor(0, ring.Color(255,255,255));
   ring.setPixelColor(8, ring.Color(255,255,255));
   ring.show();
   
   for (uint16_t i=0; i<ring.numPixels(); i++) {
+
+    if (millis() - start >= duration) {
+      animation = (animation + 1) % 5;
+      start = millis();
+      resetRing();
+      break;
+    }
     
     // turn on next pixels
     ring.setPixelColor(((i+1) % 16), ring.Color(255,255,255));
@@ -107,6 +126,13 @@ void rainbowCycle(uint8_t wait)
   uint16_t i, j;
 
   for (j=0; j<256*5; j++) {
+
+    if (millis() - start >= duration) {
+      animation = (animation + 1) % 5;
+      start = millis();
+      resetRing();
+      break;
+    }
     
     for (i=0; i<ring.numPixels(); i++) {
       ring.setPixelColor(i, Wheel(((i * 256 / ring.numPixels()) + j) & 255));
@@ -118,10 +144,18 @@ void rainbowCycle(uint8_t wait)
 
 // Theater-style chase w/ all colors
 void theaterChaseRainbow(uint8_t wait)
-{
-  for (int j=0; j<256; j++) { // cycle through all 256 colors  
-    for (int q=0; q<3; q++) {
+{  
+  for (int j=0; j<256; j++) { // cycle through all 256 colors
 
+    if (millis() - start >= duration) {
+      animation = (animation + 1) % 5;
+      start = millis();
+      resetRing();
+      break;
+    }
+      
+    for (int q=0; q<3; q++) {
+      
       for (uint16_t i=0; i<ring.numPixels(); i+=3) { // turn on every third pixel
         ring.setPixelColor(i+q, Wheel((i+j) % 255));
       }
